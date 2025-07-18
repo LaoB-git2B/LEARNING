4c106bad2e638a65e6eb3c0bdf369f1eac86f83f
LaoB-git2B <2238506018@qq.com>
2025-07-18 21:45:29 +0800

    ok

--- code/Manager/Manager.h
+++ code/Manager/Manager.h
@@ -9,8 +9,13 @@ using namespace std;
 #define C_INIT_ONE "选择学习的内容：\n1) Python\n2) C\n3) C++\n4) Java\n(请输入数字，现在为1)\n> " 
 #define E_BEGIN_ERROR "This is not what this software offers"
 #define C_BEGIN_ERROR "这不是此软件提供的内容"
-#define E_BEGIN_PYTHON_ONE ""
-#define C_BEGIN_PYTHON_ONE ""
+
+#define E_BEGIN_PYTHON_ONE "Download Python: \nThere are many versions of Python. \nAt the time of this tutorial, the latest version of Python is 3.13.5. \nLet's take the latest version as an example. First, open the https://www.python.org/ URL from your browser."
+#define C_BEGIN_PYTHON_ONE "下载Python：\nPython有许多版本。\n在此教程制作时Python的最新版本为3.13.5。\n我们以最新版本为例。\n第一，请先从浏览器中打开https://www.python.org/网址。\n"
+#define E_BEGIN_PYTHON_TWO "Second, move your mouse over the Downloads button at this point. Choose your own operating system. The latest version of Windows is displayed by default on the right. macOS users click on the macOS button on the left to choose the version that suits them."
+#define C_BEGIN_PYTHON_TWO "第二，此时将鼠标移到Downloads按钮上。选择自己的操作系统。右边默认显示出Windows的最新版本。macOS用户点击左侧macOS按钮以选择适合自己的版本。"
+#define E_BEGIN_PYTHON_THREE ""
+#define C_BEGIN_PYTHON_THREE ""
 
 class Manager {
 public:
@@ -94,10 +99,12 @@ void Manager::begin() {
         if (lanaguage == 1)
         {
             cout << E_BEGIN_PYTHON_ONE << endl;
+            cout << E_BEGIN_PYTHON_TWO << endl;
         }
         if (lanaguage == 2)
         {
             cout << C_BEGIN_PYTHON_ONE << endl;
+            cout << C_BEGIN_PYTHON_TWO << endl;
         }
     }
     else if (thing == "2")
